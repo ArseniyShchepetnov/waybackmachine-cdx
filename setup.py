24f8d15d7d670157d1bc1ad51718981a6653f3eb
Arseniy <a.shchepetnov@gmail.com>
2021-09-11 16:41:34 +0300

    fix setup (#6)

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 """Setup"""
-from distutils.core import setup
+from setuptools import setup,  find_packages
+
+install_requires = [
+    'setuptools',
+]
 
 if __name__ == '__main__':
 
     setup(name='waybackmachine_cdx',
           version='0.1.0',
           url='https://github.com/ArseniyShchepetnov/waybackmachine-cdx',
-          python_requires='>=3.8.6')
+          python_requires='>=3.8.6',
+          packages=find_packages(exclude=('tests', 'tests.*')),
+          install_requires=install_requires)
