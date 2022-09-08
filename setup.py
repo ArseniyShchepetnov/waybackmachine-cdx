e89b2ef61cbd93cbc04485c849c294f3c52e2ffa
Arseniy <a.shchepetnov@gmail.com>
2022-09-08 21:56:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     setup(name='waybackmachine_cdx',
           version='0.1.0',
           url='https://github.com/ArseniyShchepetnov/waybackmachine-cdx',
-          python_requires='>=3.8.6',
+          python_requires='>=3.7',
           packages=find_packages(exclude=('tests', 'tests.*')),
           install_requires=install_requires)
